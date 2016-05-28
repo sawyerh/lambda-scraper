1f7796f155127502273bacdcaadf9b5d90202856
Sawyer Hollenshead <s.hollenshead@gmail.com>
2016-05-27 23:32:50 -0400

    Add one more example

--- pages.js
+++ pages.js
@@ -7,5 +7,15 @@ module.exports = [
       "title": "a",
       "url": "a"
     }
+  },
+  {
+    "url": "https://twitter.com/sawyerh",
+    "keywords": [],
+    "parent": ".tweet",
+    "selectors": {
+      "title": ".tweet-text",
+      "url": ".js-permalink",
+      "name": ".fullname"
+    }
   }
 ];
